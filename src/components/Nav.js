db6a68d4306db90b61be65a52d033089e6d926d9
Milo Rastgoo <msrastgoo@gmail.com>
2020-07-27 19:32:44 -0400

    css reset added, layout defined

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,20 +3,19 @@ import React from 'react'
 const Nav = () => {
 
     return (
-        <ul className="nav justify-content-center">
-            <li className="nav-item">
-                <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/projects">Projects</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/blog">Blog</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/dashboard">Admin</a>
-            </li>
-        </ul>
+        <div className="top">
+            <div className="container row">
+                <div className="logo">
+                    <h1>Milo Rastgoo</h1>
+                </div>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/projects">Projects</a>
+                    <a href="/blog">Blog</a>
+                    <a href="/dashboard">Admin</a>
+                </nav>
+            </div>
+        </div>
     )
 }
 
