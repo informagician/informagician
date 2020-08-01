abfaec041e112d4c55c21bd64cda6edf5a49d867
Milo Rastgoo <msrastgoo@gmail.com>
2020-07-31 20:56:39 -0400

    send file functions wrote

--- src/components/admin/Media.js
+++ src/components/admin/Media.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Media = () => {
 
+    const [ file, setFile ] = useState()
+
+    const handleFile = e => {
+        setFile(e.target.files[0])
+    }
+
+    const handleUpload = e => {
+        console.log(file)
+    }
+
     return (
-        <h1>Media</h1>
+        <>
+            <h1>Media</h1>
+            <input type="file" onChange={handleFile} />
+            <input type="submit" value="Upload" onClick={handleUpload} />
+        </>
     )
 }
 
--- src/components/admin/article/New.js
+++ src/components/admin/article/New.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import 'react-quill/dist/quill.snow.css';
 
 import { Editor } from '@tinymce/tinymce-react';
 
