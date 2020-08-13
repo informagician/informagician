22051a2e84e39f56f889da71f1cba82d8e692c40
Milo Rastgoo <msrastgoo@gmail.com>
2020-08-12 23:23:12 -0400

    users migrate updated with fname and lname

--- backend/migrations/20200812224004_users.js
+++ backend/migrations/20200812224004_users.js
@@ -3,6 +3,8 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('users', tbl => {
         tbl.increments('id')
+        tbl.string('firstname',64)
+        tbl.string('lastname',64)
         tbl.string('username',64).notNullable().unique()
         tbl.string('password').notNullable()
         tbl.timestamp('created_at').notNullable()
