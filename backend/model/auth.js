7ef15f85435055de152067e7e33d4cbe4d87987e
Milo Rastgoo <msrastgoo@gmail.com>
2020-08-19 22:51:24 -0400

    user add database done, jwt,bcrypt and middleware remaining

--- backend/model/auth.js
+++ backend/model/auth.js
@@ -6,9 +6,20 @@ module.exports = {
 }
 
 function list() {
-    
+    return db('users')
+        .select('*')
 }
 
 function add(user) {
+    return db('users')
+        .insert(user, 'id')
+        .then(ids => {
+            return findById(ids)
+        })
+}
 
+function findById(id){
+    return db('users')
+        .select('*')
+        .where({id})
 }
--- backend/router/auth.js
+++ backend/router/auth.js
@@ -9,7 +9,11 @@ router.get('/', (req,res) => {
 
 router.post('/add', (req,res) => {
     let userInfo = req.body
-    res.status(200).json({message:`success`})
+    Auth.add(userInfo)
+        .then(user => {
+            res.status(200).json({message:`${user}`})
+        })
+        .catch(err => console.log(err))
 })
 
 module.exports = router
