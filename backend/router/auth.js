5fd4724aa6ce3e82ad10a7eb2ac3d357487547f3
Milo Rastgoo <msrastgoo@gmail.com>
2020-08-20 22:04:20 -0400

    restricted middleware for backend

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -2,14 +2,15 @@ const router = require('express').Router()
 const Auth = require('../model/auth')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const restricted = require('../middleware/restricted')
 
 router.get('/', (req,res) => {
     res.status(200).json({message:'success'})
 })
 
 router.post('/add', (req,res) => {
-    let userInfo = req.body
-    Auth.add(userInfo)
+    let user = req.body
+    Auth.add(user)
         .then(user => {
             res.status(200).json({message:`${user}`})
         })
@@ -17,3 +18,16 @@ router.post('/add', (req,res) => {
 })
 
 module.exports = router
+
+function generateToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username
+    }
+
+    const options = {
+        expiresIn: '1h'
+    }
+
+    return jwt.sign(payload, jwtsecret, options)
+}
